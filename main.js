43e9182955634db94b8e316af3df7b1d3db07061
Dioguinn06 <167586248+Dioguinn06@users.noreply.github.com>
2024-06-14 12:16:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixa
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-Resultado');
+const textoResultado = document.querySelector('.texto-Resultado');
+
+const perguntas

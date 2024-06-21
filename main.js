22eb826654ee47f6723a0250cd65fc038ad1f015
Dioguinn06 <167586248+Dioguinn06@users.noreply.github.com>
2024-06-21 12:24:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,36 @@ const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-Resultado');
 const textoResultado = document.querySelector('.texto-Resultado');
 
-const perguntas
+const perguntas = [
+    {
+        enunciado: "Insira o enunciado da pergunta 1",
+        alternativa: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Insira o enunciado da Pergunta 2",
+        alternativa: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Insira o enunciado da Pergunta 3",
+        alternativa: ["Alternativa","Alternativa 2"]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta (){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+mostraPergunta();
+
+function mostraAlternativas() {
+    for (const alternativas of perguntaAtual.alternativas) {
+        const botaoAlternativas = 
+        document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
